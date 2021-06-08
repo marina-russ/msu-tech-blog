ffa76fd99228e451c072ab4f81e3270c55ebf00a
Marina Russ <marina@cameystudios.com>
2021-06-08 11:30:59 -0400

    changed const sequelize to ternary notation

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,14 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-let sequelize;
-
-// Connects to database and passes your MySQL info for username and password
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
 
 module.exports = sequelize;
