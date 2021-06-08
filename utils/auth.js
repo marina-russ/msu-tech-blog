c9ab232ca3b2990ab749b30d32591dba2e75f7ae
Marina Russ <marina@cameystudios.com>
2021-06-08 11:47:03 -0400

    changes loginIn to userId in auth, updates format_date in helpers

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 const withAuth = (req, res, next) => {
   // If the user is not logged in, redirect the request to the login route
-  if (!req.session.loggedIn) {
+  if (!req.session.userId) {
     res.redirect('/login');
   } else {
     next();
@@ -8,3 +8,4 @@ const withAuth = (req, res, next) => {
 };
 
 module.exports = withAuth;
+
